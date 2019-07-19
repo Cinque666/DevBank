a3065aa4b7b119d7697f0989bf7e5c182b09da39
Cinque666 <roma.zamoiski@gmail.com>
2019-07-19 16:30:43 +0300

    optimized imports

--- src/main/java/com/by/dev/ramanzamoiski/controller/Controller.java
+++ src/main/java/com/by/dev/ramanzamoiski/controller/Controller.java
@@ -10,12 +10,10 @@ import org.apache.logging.log4j.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 //@WebServlet(urlPatterns = "/users")
 public class Controller extends HttpServlet {
